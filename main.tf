75b8e052130e2c73aa8cde8eab2964ae5a244567
potturi319 <67281201+potturi319@users.noreply.github.com>
2021-12-24 13:04:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 provider "aws"  {
     region = "eu-north-1"
+    version = "~> 2.38.0"
 }
 resource "aws_instance" "ec2-sumanth" {
     ami = "ami-05760f62e0b3eab56"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
     key_name = "DevOps"
     tags = {
         Name = "Terraform EC2"

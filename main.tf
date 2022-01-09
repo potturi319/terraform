8b94efd7a4be5213dc3637b6e8c2c76046194ba7
potturi319 <67281201+potturi319@users.noreply.github.com>
2022-01-09 10:00:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws"  {
 resource "aws_instance" "ec2-sumanth" {
     ami = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
+    iam_instance_profile = "${aws_iam_instance_profile.admin_profile.name}"
     key_name = "edu"
     tags = {
         Name = "Terraform EC2"

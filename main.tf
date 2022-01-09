580768188f8a43f47c672e8bfbf0937d68ff294c
potturi319 <67281201+potturi319@users.noreply.github.com>
2022-01-09 09:49:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws"  {
-    region = "eu-north-1"
+    region = "us-east-2"
     version = "~> 2.38.0"
 }
 resource "aws_instance" "ec2-sumanth" {
-    ami = "ami-05760f62e0b3eab56"
-    instance_type = "t3.micro"
-    key_name = "DevOps"
+    ami = "ami-0fb653ca2d3203ac1"
+    instance_type = "t2.micro"
+    key_name = "edu"
     tags = {
         Name = "Terraform EC2"
     }
